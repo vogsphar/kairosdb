377bf726b3d577c900fad4e043a064deb1916e97
vogsphar <vogsphar>
2017-11-14 13:19:14 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # kairosdb
+
+## minimal configuration
+
+``` bash
+IP=$(hostname -I) # 
+docker run -it -e CASSANDRA_BROADCAST_ADDRESS=$IP -e CASSANDRA_START_RPC=true -p 9160:9160  --name some-cassandra1 cassandra
+docker run --name some-kairos1 -d -p 8080:8080 -e "CASSANDRA_HOST_LIST=$IP" vogsphar/kairosdb
+```
+
+now you can point your browser to http://localhost:8080
+
+## changing keyspace name
+
+using `-e "CASSANDRA_KEYSPACE=bob"` can be used to change the default keyspace name "kairosdb" to a "bob".
+
+## build custom image
+``` bash
+git clone https://github.com/vogsphar/kairosdb
+cd kairosdb
+docker build --build-arg SRC=https://github.com/kairosdb/kairosdb/releases/download/v1.1.1/kairosdb-x.x.x-x.tar.gz -t my-kairosdb -f Dockerfile.kairosdb .
+docker run --name my-kairos1 -d -p 8080:8080 -e "CASSANDRA_HOST_LIST=$IP" my-kairosdb
+```
---
+++ kairosdb.docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+CONF=/opt/kairosdb/conf/kairosdb.properties
+function conf
+{
+  sed -i -e "s/^$1=.*/$1=$2/" $CONF
+}
+if [ -n "$CASSANDRA_KEYSPACE" ]; then
+# kairosdb.datastore.cassandra.hector.loadBalancingPolicy 
+	conf kairosdb.datastore.cassandra.keyspace "$CASSANDRA_KEYSPACE"
+fi
+if [ -n "$CASSANDRA_HOST_LIST" ]; then
+# kairosdb.datastore.cassandra.hector.loadBalancingPolicy 
+	conf kairosdb.datastore.cassandra.read_consistency_level ONE
+	conf kairosdb.datastore.cassandra.write_consistency_level ONE
+	conf kairosdb.datastore.cassandra.replication_factor 1
+	conf kairosdb.service.datastore org.kairosdb.datastore.cassandra.CassandraModule
+	conf kairosdb.query_cache.cache_file_cleaner_schedule "0 *\\/30 * * * ?"
+	conf kairosdb.datastore.cassandra.write_buffer_max_size 3000
+	conf kairosdb.datastore.cassandra.host_list "$CASSANDRA_HOST_LIST"
+fi
+cat $CONF
+exec /opt/kairosdb/bin/kairosdb.sh run
